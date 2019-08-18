a3fcad880866051193594446c9587e6d96fc9821
masatotoro <36653513+masatotoro@users.noreply.github.com>
2019-08-19 04:35:43 +0900

    create spring project

---
+++ src/main/java/jp/gihyo/wdpress/advance/SpringBootCrawlerApplication.java
@@ -0,0 +1,13 @@
+package jp.gihyo.wdpress.advance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrawlerApplication.class, args);
+	}
+
+}
