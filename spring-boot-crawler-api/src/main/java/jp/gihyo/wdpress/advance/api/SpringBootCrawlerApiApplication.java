7a7d7d1fdaf0b6ff128977da711e535e3e747578
masatotoro <36653513+masatotoro@users.noreply.github.com>
2019-08-19 22:11:31 +0900

    add spring-boot-crawler-api

--- spring-boot-crawler-api/src/main/java/jp/gihyo/wdpress/advance/api/SpringBootCrawlerApiApplication.java
+++ spring-boot-crawler-api/src/main/java/jp/gihyo/wdpress/advance/api/SpringBootCrawlerApiApplication.java
@@ -1,13 +1,13 @@
-package jp.gihyo.wdpress.advance;
+package jp.gihyo.wdpress.advance.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootCrawlerApplication {
+public class SpringBootCrawlerApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrawlerApplication.class, args);
+		SpringApplication.run(SpringBootCrawlerApiApplication.class, args);
 	}
 
 }
--- spring-boot-crawler-api/src/test/java/jp/gihyo/wdpress/advance/api/SpringBootCrawlerApiApplicationTests.java
+++ spring-boot-crawler-api/src/test/java/jp/gihyo/wdpress/advance/api/SpringBootCrawlerApiApplicationTests.java
@@ -1,4 +1,4 @@
-package jp.gihyo.wdpress.advance;
+package jp.gihyo.wdpress.advance.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootCrawlerApplicationTests {
+public class SpringBootCrawlerApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
