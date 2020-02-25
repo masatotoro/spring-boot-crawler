5087c1499c1ba91f3480a06f5cf5b5459f0628c1
masatotoro <36653513+masatotoro@users.noreply.github.com>
2020-02-26 01:15:17 +0900

    edit test setting

--- spring-boot-lib-activemq/src/test/java/jp/gihyo/wbpress/advance/lib/activemq/QiitaActivemqApplicationTests.java
+++ spring-boot-lib-activemq/src/test/java/jp/gihyo/wbpress/advance/lib/activemq/QiitaActivemqApplicationTests.java
@@ -1,18 +1,13 @@
 package jp.gihyo.wbpress.advance.lib.activemq;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@Ignore
-@RunWith(SpringRunner.class)
+// @Ignore
+// @RunWith(SpringRunner.class)
 @SpringBootTest
 public class QiitaActivemqApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
-
+  @Test
+  public void contextLoads() {}
 }
