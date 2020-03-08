c9238ceda7f37029750e51f0c8e0103d8930bad4
masatotoro <36653513+masatotoro@users.noreply.github.com>
2020-03-09 00:18:19 +0900

    add spring-cloud-eureka

--- spring-boot-crawler-api/src/main/java/jp/gihyo/wdpress/advance/api/SpringBootCrawlerApiApplication.java
+++ spring-boot-crawler-api/src/main/java/jp/gihyo/wdpress/advance/api/SpringBootCrawlerApiApplication.java
@@ -2,7 +2,9 @@ package jp.gihyo.wdpress.advance.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringBootCrawlerApiApplication {
 
---
+++ spring-cloud-eureka/src/test/java/jp/gihyo/wdbpress/advane/eureka/SpringCloudEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gihyo.wdbpress.advane.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
